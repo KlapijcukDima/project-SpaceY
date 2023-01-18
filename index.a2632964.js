c86fe2a002904794904398ad3889ee4818642d5a
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-01-18 09:25:10 +0000

    Deploying to gh-pages from @ KlapijcukDima/project-SpaceY@8bb238c6cd0dd4cfd5774b2be8f95d1f90f8ffec 🚀

---
+++ index.a2632964.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-subscribe-open]"),closeModalBtn:document.querySelectorAll("[data-modal-subscribe-close]"),modal:document.querySelector("[data-modal-subscribe]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.forEach((function(e){e.addEventListener("click",o)}))}();
+//# sourceMappingURL=index.a2632964.js.map
