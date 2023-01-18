5f93518fa1b69ccf1367e21867b6067602957095
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-01-18 19:33:59 +0000

    Deploying to gh-pages from @ KlapijcukDima/project-SpaceY@7fb499f4e11670d3880d37c80e323a6dc8b58fcb 🚀

--- index.284ade97.js
+++
@@ -1,2 +0,0 @@
-var fields=document.querySelectorAll(".contact-form-field"),submitButton=document.querySelector(".modal-btn");(fields=Array.from(fields)).forEach((function(e){e.addEventListener("keyup",(function(){submitButton.disabled=!fields.every((function(e){return e.value}))}))}));
-//# sourceMappingURL=index.284ade97.js.map
--- index.a2632964.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-subscribe-open]"),closeModalBtn:document.querySelectorAll("[data-modal-subscribe-close]"),modal:document.querySelector("[data-modal-subscribe]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.forEach((function(e){e.addEventListener("click",o)}))}();
-//# sourceMappingURL=index.a2632964.js.map
